78d7bd733067043300a851129a18063ea5d9dcfb
Sergey Kochubey <sko4ubey@gmail.com>
2024-11-25 01:17:32 +0300

    fix PostChangeService

--- app/services/post_state_change_service.rb
+++ app/services/post_state_change_service.rb
@@ -1,10 +1,6 @@
 class PostStateChangeService
   def self.change_state(post_id, state_action)
-    post = Post.includes(:user).find(post_id)
-    user = post.user
-
-    policy = PostPolicy.new(user, post)
-    return unless policy.public_send("#{state_action}?")
+    post = Post.find(post_id)
 
     post.change_state(state_action)
   rescue ActiveRecord::RecordNotFound => exception
