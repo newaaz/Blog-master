29e59df9e6061bf8352da1866eb08cc4c2098995
Sergey Kochubey <sko4ubey@gmail.com>
2024-12-08 19:44:14 +0300

    design

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+document.addEventListener("turbo:load", () => {
+    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    tooltipTriggerList.map((tooltipTriggerEl) => {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+})
